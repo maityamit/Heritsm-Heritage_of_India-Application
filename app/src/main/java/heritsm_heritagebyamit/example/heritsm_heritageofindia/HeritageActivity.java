298fd3b482eefc3533893df383265fc5d6eb675f
AMIT <maityamit9800@gmail.com>
2021-11-02 22:57:06 +0530

    some changes

---
+++ app/src/main/java/heritsm_heritagebyamit/example/heritsm_heritageofindia/HeritageActivity.java
@@ -0,0 +1,14 @@
+package heritsm_heritagebyamit.example.heritsm_heritageofindia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HeritageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heritage);
+    }
+}
