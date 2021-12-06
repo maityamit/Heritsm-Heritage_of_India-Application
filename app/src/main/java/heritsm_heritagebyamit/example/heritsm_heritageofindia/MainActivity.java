8e906c751a2c0c09086633761eba445a0be3843d
AMIT <maityamit9800@gmail.com>
2021-12-06 22:55:59 +0530

    some changes

--- app/src/main/java/heritsm_heritagebyamit/example/heritsm_heritageofindia/MainActivity.java
+++ app/src/main/java/heritsm_heritagebyamit/example/heritsm_heritageofindia/MainActivity.java
@@ -2,13 +2,27 @@ package heritsm_heritagebyamit.example.heritsm_heritageofindia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        relativeLayout = findViewById(R.id.heritage_all);
+        relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,HeritageListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/heritsm_heritagebyamit/example/heritsm_heritageofindia/productModel.java
@@ -0,0 +1,58 @@
+package heritsm_heritagebyamit.example.heritsm_heritageofindia;
+
+public class productModel {
+    public String description, image , location, period,place_name;
+
+
+    public productModel(String Description, String Image, String Location,String Period,String Place_Name) {
+        this.description = Description;
+        this.image = Image;
+        this.location = Location;
+        this.period=Period;
+        this.place_name = Place_Name;
+
+    }
+
+    public productModel() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(String period) {
+        this.period = period;
+    }
+
+    public String getPlace_name() {
+        return place_name;
+    }
+
+    public void setPlace_name(String place_name) {
+        this.place_name = place_name;
+    }
+}
